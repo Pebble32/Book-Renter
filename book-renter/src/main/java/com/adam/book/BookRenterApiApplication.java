1bf47108e83befbe2489e75bc4d5b2ffdce5dfef
Adam Postek <bjkk32@gmail.com>
2024-07-28 21:33:15 +0000

    Small fix

---
+++ book-renter/src/main/java/com/adam/book/BookRenterApiApplication.java
@@ -0,0 +1,13 @@
+package com.adam.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRenterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRenterApiApplication.class, args);
+	}
+
+}
---
+++ book-renter/src/test/java/com/adam/book/BookRenterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adam.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRenterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
