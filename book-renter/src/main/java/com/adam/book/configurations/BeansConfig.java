afec94eb188bff6b1fc4567a6252ccca36ee9ce9
adamp <bjkk32@gmail.com>
2024-08-06 13:28:43 +0000

    Fix imports

--- book-renter/src/main/java/com/adam/book/configurations/BeansConfig.java
+++ book-renter/src/main/java/com/adam/book/configurations/BeansConfig.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -19,7 +18,11 @@ import org.springframework.web.filter.CorsFilter;
 import java.util.Arrays;
 import java.util.Collections;
 
-import static org.springframework.http.HttpHeaders.*;
+import static org.springframework.http.HttpHeaders.ACCEPT;
+import static org.springframework.http.HttpHeaders.ORIGIN;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
 
 @Configuration
 @RequiredArgsConstructor
