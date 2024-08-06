54aa2655974fea52d6a1e80ec16f25b44e5ba7cc
adamp <bjkk32@gmail.com>
2024-08-06 11:41:24 +0000

    OpenApi impl

--- book-renter/src/main/java/com/adam/book/configurations/SecurityConfig.java
+++ book-renter/src/main/java/com/adam/book/configurations/SecurityConfig.java
@@ -26,27 +26,31 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors(withDefaults())
+        http
+                .cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
-                            req.requestMatchers(
-                                    "/auth/**",
-                                    "/v3/api-docs",
-                                    "/v3/api-docs/**",
-                                    "swagger-resources",
-                                    "swagger-resources/**",
-                                    "configuration/ui",
-                                    "configuration/security",
-                                    "swagger-ui/**",
-                                    "webjars/**",
-                                    "/swagger-ui.html"
-                            ).permitAll()
-                                    .anyRequest()
-                                        .authenticated()
-                        )
+                        req.requestMatchers(
+                                        "/auth/**",
+                                        "/v2/api-docs",
+                                        "/v3/api-docs",
+                                        "/v3/api-docs/**",
+                                        "/swagger-resources",
+                                        "/swagger-resources/**",
+                                        "/configuration/ui",
+                                        "/configuration/security",
+                                        "/swagger-ui/**",
+                                        "/webjars/**",
+                                        "/swagger-ui.html"
+                                )
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-            return http.build();
+
+        return http.build();
     }
 }
