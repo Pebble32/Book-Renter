7fca1887f46e5e2ae76876e6f235da504ba1905d
adamp <bjkk32@gmail.com>
2024-08-12 11:20:38 +0000

    Registration impl

--- br-ur/src/app/pages/register/register.component.ts
+++ br-ur/src/app/pages/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RegistrationRequest} from '../../services/models/registration-request'
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/services/authentication.service";
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  registrationRequest: RegistrationRequest = {email: '', password: '', firstName: '', lastName: ''};
+  errorMessage: Array<string> = [];
 
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService,
+  ) {
+  }
+
+  register() {
+    this.errorMessage = [];
+    this.authenticationService.register({
+      body: this.registrationRequest
+    }).subscribe({
+      next: (res) => {
+        this.router.navigate(['activate-account']);
+      },
+      error: (err) => {
+        this.errorMessage = err.error.validationErrors;
+      }
+    })
+
+  }
+
+  toLogin() {
+    this.router.navigate(['login']);
+  }
 }
