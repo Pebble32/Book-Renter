5a9397f1a0df68fd40f90954c249fac569e4a021
adamp <bjkk32@gmail.com>
2024-08-21 08:39:49 +0000

    Impl pagination

--- br-ur/src/app/modules/book/pages/book-list/book-list.component.ts
+++ br-ur/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {BookService} from "../../../../services/services/book.service";
 import {Router} from "@angular/router";
 import {PageResponseBookResponse} from "../../../../services/models/page-response-book-response";
+import {BookResponse} from "../../../../services/models/book-response";
 
 @Component({
   selector: 'app-book-list',
@@ -12,6 +13,8 @@ export class BookListComponent implements OnInit {
   bookResponse: PageResponseBookResponse = {}
   page = 0;
   size = 5;
+  message = '';
+  level = 'success'
 
   constructor(
     private bookService: BookService,
@@ -35,26 +38,48 @@ export class BookListComponent implements OnInit {
   }
 
   goToFirstPage() {
-
+    this.page = 0;
+    this.findAllBooks();
   }
 
   goToPreviousPage() {
-
+    this.page--;
+    this.findAllBooks();
   }
 
-  goToPage(index: number) {
-
+  goToPage(page: number) {
+    this.page = page;
+    this.findAllBooks();
   }
 
   goToNextPage() {
-
+    this.page++;
+    this.findAllBooks();
   }
 
   goToLastPage() {
-
+    this.page = this.bookResponse.totalPages as number -1;
+    this.findAllBooks();
   }
 
   get isLastPage(): boolean {
     return this.page == this.bookResponse.totalPages as number - 1;
+  }
+
+  borrowBook(book: BookResponse){
+    this.message = '';
+    this.bookService.borrowBook({
+      'book-id': book.id as number
+    }).subscribe({
+      next: () => {
+        this.level = 'success'
+        this.message = 'Book added to your borrow list'
+      },
+      error: err => {
+        console.log(err);
+        this.level = 'error'
+        this.message = err.error.errors;
+      }
+    })
   }
 }
